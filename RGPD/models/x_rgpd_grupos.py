8de36bda1659c50b4102abddb2c653959f4585d7
Antonio Cánovas <antonio.canovas@ingenieriacloud.com>
2019-05-24 15:28:38 +0200

    Error al escribir column, corregido

--- RGPD/models/x_rgpd_grupos.py
+++ RGPD/models/x_rgpd_grupos.py
@@ -9,7 +9,7 @@ class x_rgpd_grupos(models.Model):
 
     x_name= fields.Char(string="Name", required=True)
     x_contacto_ids = fields.Many2many('res.partner', domain=[('is_company','=',False)],
-                                      relation='x_rgpd_grupo_contacto_rel', cloumn1='x_grupo_id' ,clolum2='x_contacto_id', string="Contacto")
+                                      relation='x_rgpd_grupo_contacto_rel', column1='x_grupo_id' ,column2='x_contacto_id', string="Contacto")
     x_empresa_id = fields.Many2many('res.partner', domain=[('is_company', '=', True)])
     #x_grupo_id = fields.Many2many('res.partner', domain=[('is_company', '=', False)])
     x_estado = fields.Selection([('borrador', 'Borrador'), ('activo', 'Activo'), ('archivado', 'Archivado')])
