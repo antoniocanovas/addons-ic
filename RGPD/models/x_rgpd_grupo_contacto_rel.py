655e02f0658eb7b25f8863987c9f1e95234558c2
Antonio Cánovas <antonio.canovas@ingenieriacloud.com>
2019-05-24 17:31:24 +0200

    Quito parámetros x_name, no hace caso.

--- RGPD/models/x_rgpd_grupo_contacto_rel.py
+++ RGPD/models/x_rgpd_grupo_contacto_rel.py
@@ -7,8 +7,8 @@ class x_rgpd_grupo_contacto_rel(models.Model):
     _description = "RGPD rel"
 
 
-    x_name= fields.Char(string="Name", required=False, stored=False)
-    # Queda pendiente poner x_name = nombre_grupo + " => " + nombre_contacto !!
+    x_name= fields.Char(string="Name")
+    # Queda pendiente poner x_name = nombre_grupo + " => " + nombre_contacto !! y además que no sea almacenado.
     x_grupo_id= fields.Many2one('x_rgpd_grupos')
     x_contacto_id = fields.Many2one('res.partner')
     x_empresa_id = fields.Many2one('res.partner',related='x_contacto_id.parent_id',stored=False,string='Empresa')
