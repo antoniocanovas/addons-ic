1f014dad11909cea788c25913604159a121dbb35
pedroguirao <pedro.guirao@ingenieriacloud.com>
2019-11-27 09:14:58 -0800

    sin view

--- project_task_recurrency/models/project_task.py
+++ project_task_recurrency/models/project_task.py
@@ -1,4 +1,4 @@
-# Copyright
+# Copyright Serincloud
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 
@@ -9,10 +9,12 @@ class ProjectTask(models.Model):
     _inherit = 'project.task'
 
 
-    rec_type = fields.Selection('Tipo',selection=[('fechabase','En base a la fecha base'),('fechaarchivado','A partir de la fecha de archivado')])
+    rec_type = fields.Selection([
+        ('fechabase','En base a la fecha base'),('fechaarchivado','A partir de la fecha de archivado')]
+    ,string='Tipo')
     rec_qty = fields.Integer('Cada')
     rec_date = fields.Date('Fecha base')
-    rec_period = fields.Selection('Periodo', selection=[('dias','Días'),('meses','Meses')])
+    rec_period = fields.Selection([('dias','Días'),('meses','Meses')],string='Periodo')
     rec_next_task_id = fields.Many2one('project.task',string='Próx tarea')
 
 
