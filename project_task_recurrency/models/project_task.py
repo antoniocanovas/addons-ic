368f586672c4e4e97660e3fb3956c53c39715393
pedroguirao <pedro.guirao@ingenieriacloud.com>
2019-11-28 07:32:47 -0800

    Selection Update

--- project_task_recurrency/models/project_task.py
+++ project_task_recurrency/models/project_task.py
@@ -15,7 +15,7 @@ class ProjectTask(models.Model):
     rec_qty = fields.Integer('Cada')
     rec_date = fields.Date('Fecha base')
     rec_period = fields.Selection(
-        [('dias','Días'),('meses','Meses')]
+        [('dias','Días'),('meses','Meses'),('findemes','El último día del mes')]
         ,string='Periodo')
     rec_next_task_id = fields.Many2one('project.task',string='Próx tarea')
 
