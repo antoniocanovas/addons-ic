88bec936ecca1233c2949a88da18f73cdce26afb
pedroguirao <pedro.guirao@ingenieriacloud.com>
2019-11-29 05:39:38 -0800

    user_permissions menu model change 3

--- user_manager_permission_limit/__manifest__.py
+++ user_manager_permission_limit/__manifest__.py
@@ -1,25 +1,19 @@
 {
     'name': "user manager permision limit",
     'summary': """
-       Plantilla por defecto para nuevas Empresas en multiempresa, se aplica al crear.
+       
         """,
     'author': "Pedro Guirao",
     'license': 'AGPL-3',
     'website': "https://ingenieriacloud.com",
     'category': 'Tools',
-    'version': '12.0.3.0.0',
+    'version': '12.0.4.0.0',
     'depends': [
 
     ],
     'data': [
-        #'data/default_rules.xml',
-        #'data/default_actions.xml',
-        #'views/view.xml',
         'views/res_user_groups.xml',
-        'security/user_groups.xml',
-        #'security/ir.model.access.csv',
-        #'data/default_data.xml',
-
+        'security/menu_access.xml',
     ],
     'installable': True,
     'application': True,
