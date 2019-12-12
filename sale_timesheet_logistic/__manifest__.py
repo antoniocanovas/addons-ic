b0e830cd918966de179fd77fb8287c8edf269235
pedroguirao <pedro.guirao@ingenieriacloud.com>
2019-12-12 04:16:02 -0800

    fsm.view

--- sale_timesheet_logistic/__manifest__.py
+++ sale_timesheet_logistic/__manifest__.py
@@ -19,7 +19,7 @@
         'views/res_partner_views.xml',
         'wizards/create_route_views.xml',
         'wizards/merge_route_views.xml',
-        'project_task_form_fsm_views.xml',
+        'views/project_task_form_fsm_views.xml',
         'data/server_action.xml',
     ],
     'installable': True,
