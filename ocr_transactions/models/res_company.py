4bc29ba4f25581771ffb1a0bad941c82092a797c
pedroguirao <pedroguirao@ingenieriacloud.com>
2020-07-10 11:07:58 +0200

    random vat bug

--- ocr_transactions/models/res_company.py
+++ ocr_transactions/models/res_company.py
@@ -64,7 +64,7 @@ class ResCompany(models.Model):
             return header
 
     @api.multi
-    def random_with_N_digits(n):
+    def random_with_N_digits(self, n):
         range_start = 10 ** (n - 1)
         range_end = (10 ** n) - 1
         return randint(range_start, range_end)
