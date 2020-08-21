7bda7ab74bd09f9dd18ad0aece0d545176e32815
Antonio Cánovas <antonio.canovas@ingenieriacloud.com>
2020-08-21 19:19:18 +0200

    Add analytic_id to fleet_vehicle

--- fieldservice_logistic_analysis/models/fsm_logistic_fleet.py
+++ fieldservice_logistic_analysis/models/fsm_logistic_fleet.py
@@ -6,3 +6,7 @@ class LogisticFleetVehicle(models.Model):
 
 
     cost_per_km = fields.Float('Coste/KM')
+    
+    analytic_id = fields.Many2one(
+        comodel_name='account.analytic.line',
+        string='Anlytic Account',
