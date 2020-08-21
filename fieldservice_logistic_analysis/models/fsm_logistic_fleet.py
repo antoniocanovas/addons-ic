3a3a91cd12293b43e0b8bf0350a1c56280fc9729
Antonio Cánovas <antonio.canovas@ingenieriacloud.com>
2020-08-21 19:29:47 +0200

    Lo dejo como estaba ...

--- fieldservice_logistic_analysis/models/fsm_logistic_fleet.py
+++ fieldservice_logistic_analysis/models/fsm_logistic_fleet.py
@@ -6,8 +6,3 @@ class LogisticFleetVehicle(models.Model):
 
 
     cost_per_km = fields.Float('Coste/KM')
-    
-    analytic_id = fields.Many2one(
-        comodel_name='account.analytic.account',
-        string='Anlytic Account',
-    )
