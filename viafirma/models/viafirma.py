4c91a289a4ce0e6d13b1cc6b1eda63c03543a94d
lmmar-ic <luismi@ingenieriacloud.com>
2020-11-19 16:26:31 +0100

    CAmbios finales status y vistas en modelo viafirma

--- viafirma/models/viafirma.py
+++ viafirma/models/viafirma.py
@@ -76,7 +76,7 @@ class Viafirma(models.Model):
         viafirmas =self.env['viafirma'].search([])
         for via in viafirmas:
             if via.status != 'RESPONSED':
-                self.status_response_firmweb()
+                self.status_response_firmweb(via)
 
     def get_uploader_header(self):
 
