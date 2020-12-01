f4d1c9614d0c40563d2297da82d3585d9ad3b0da
pedroguirao <pedroguirao@ingenieriacloud.com>
2020-12-01 10:04:13 +0100

    mig-13.0

---
+++ user_manager_permission_limit/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': "user manager permision limit",
+    'summary': """
+        Oculta la opción de permisos de Administración para el grupo user manager, solo tendrán acceso aquellos
+        usuarios con "base.group_system".
+        """,
+    'author': "Pedro Guirao",
+    'license': 'AGPL-3',
+    'website': "https://ingenieriacloud.com",
+    'category': 'Tools',
+    'version': '12.0.4.0.0',
+    'depends': [
+
+    ],
+    'data': [
+        'views/res_user_groups.xml',
+        'security/menu_access.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
