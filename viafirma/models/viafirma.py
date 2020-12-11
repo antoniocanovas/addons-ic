7998a26709cfccb61cfd1d318243f661aefbcf46
lmmar-ic <luismi@ingenieriacloud.com>
2020-12-11 09:36:19 +0100

    Arreglos finales

--- viafirma/models/viafirma.py
+++ viafirma/models/viafirma.py
@@ -203,7 +203,8 @@ class Viafirma(models.Model):
                     recipient_n = {
                         "type": "SIGNATURE",
                         "id": "evidence_" + str(numEvidence),
-                        "enabledExpression": str("formItemIsNotEmpty('{{FIRMANTE_") + str(0) + str(y) + "_KEY}}','') ",
+                        #"enabledExpression": str("formItemIsNotEmpty('{{FIRMANTE_") + str(0) + str(y) + "_KEY}}','') ",
+                        "enabledExpression": str("formItemIsNotEmpty('{{FIRMANTE_") + str(0) + str(y) + "_NAME}}','') ",
                         "enabled": "true",
                         "visible": "true",
                         #"helpText": "{{FIRMANTE_" + str(x) + str(y) + "_NAME}}",
