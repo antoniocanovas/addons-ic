53e5c814c004db32c090ad3f83454aa2de5b9669
lmmar-ic <luismi@ingenieriacloud.com>
2021-02-01 11:25:49 +0100

    Nuevo modulo sale_order_split_notes

---
+++ sale_order_split_notes/models/sale.py
@@ -0,0 +1,19 @@
+# Copyright
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models, api
+from odoo.exceptions import ValidationError
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.multi
+    def split_note_lines(self):
+        for li in self.order_line:
+            if li.display_type == 'line_note':
+                texto = li.name
+                array = texto.splitlines()
+                for a in array:
+                    nueva = self.env['sale.order.line'].create(
+                        {'display_type': 'line_note', 'name': a, 'order_id': self.id, 'sequence': li.sequence})
+                li.unlink()
