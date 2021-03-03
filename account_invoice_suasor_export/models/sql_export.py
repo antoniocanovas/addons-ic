5ad126f3b48910afc5a403b4d23c73c93bc0b60e
pedro@serincloud.com <pedro@serincloud.com>
2021-03-03 10:35:50 +0100

    Suasor & Due_date

--- account_invoice_suasor_export/models/sql_export.py
+++ account_invoice_suasor_export/models/sql_export.py
@@ -9,7 +9,7 @@ _logger = logging.getLogger(__name__)
 
 
 class SqlExportSuasor(models.Model):
-    _inherit = 'sql.export'
+    _inherit = 'sql.file.wizard'
 
     sql_date_compare = fields.Date(
         'Fecha',
