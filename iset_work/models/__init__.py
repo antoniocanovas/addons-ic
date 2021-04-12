73d206f81c79b6e7db9ed91d987dd9180be92d28
pedro@serincloud.com <pedro@serincloud.com>
2021-04-12 17:11:45 +0200

    iset_work

---
+++ iset_work/models/__init__.py
@@ -0,0 +1,7 @@
+from . import iset_work
+from . import iset_work_employee
+from . import project
+from . import sale_order
+from . import stock_location
+
+
---
+++ iset_work/models/iset_work_employee.py
@@ -0,0 +1,14 @@
+from odoo import _, api, fields, models
+
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class iSetWorkEmployee(models.Model):
+    _name = 'iset.work.employee'
+    _description = 'iSetWorkEmployee'
+
+    employee_id = fields.Many2one('hr.employee', 'Employee')
+    iset_work_id = fields.Many2one('iset.work', 'iSet Work')
+    function_ids = fields.Many2many('hr.employee', 'job_id', 'Function')
+
