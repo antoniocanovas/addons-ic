30fc890c50c843f127fbec3bf07d4047d55ab7e0
pedro@serincloud.com <pedro@serincloud.com>
2021-04-26 09:11:03 +0200

    purchase_repair

--- purchase_repair/models/repair.py
+++ purchase_repair/models/repair.py
@@ -15,7 +15,12 @@ class RepairOrder(models.Model):
         )
         self.purchase_order_count = len(results)
 
-    purchase_order_count = fields.Integer('Lines', compute=_get_purchase_order_count)
+    purchase_order_count = fields.Integer('Purchases', compute=_get_purchase_order_count)
+
+    def action_view_purchases(self):
+        action = self.env.ref(
+            'purchase_repair.action_view_purchases').read()[0]
+        return action
 
 
 
