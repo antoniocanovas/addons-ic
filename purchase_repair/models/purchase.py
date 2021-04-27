8a35c7f6ea4fe2189377996b53c39309738b7005
pedro@serincloud.com <pedro@serincloud.com>
2021-04-27 12:04:43 +0200

    purchase_repair

--- purchase_repair/models/purchase.py
+++ purchase_repair/models/purchase.py
@@ -7,4 +7,8 @@ _logger = logging.getLogger(__name__)
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
-    repair_id = fields.Many2one('repair.order', string="Reparaciones")
+    repair_id = fields.Many2one(
+        'repair.order',
+        string="Reparaciones",
+        domain="[('state','in',['confirmed','under_repair','ready'])]"
+    )
