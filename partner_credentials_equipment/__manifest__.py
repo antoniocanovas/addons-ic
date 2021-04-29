825c6503c33df0dd6ae3aa5526269e152e5e6852
pedro@serincloud.com <pedro@serincloud.com>
2021-04-29 10:30:44 +0200

    v13 credentials

---
+++ partner_credentials_equipment/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': "partner credentials Equipment",
+    'summary': """
+        Añade un nuevo menú para añadir dispositivos y vincularlos a las credenciales creadas.
+        """,
+    'author': "Pedro Guirao",
+    'license': 'AGPL-3',
+    'website': "https://ingenieriacloud.com",
+    'category': 'Tools',
+    'version': '13.0.1.0.1',
+    'depends': [
+        'partner_credentials',
+        'maintenance',
+    ],
+    'data': [
+        'views/views.xml',
+        #'views/view_menu.xml',
+        #'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'application': True,
+}
