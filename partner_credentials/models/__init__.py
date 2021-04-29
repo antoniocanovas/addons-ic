029c151010b0d19ecd62552f1e6f2bb75bb23217
pedro@serincloud.com <pedro@serincloud.com>
2021-04-29 16:42:20 +0200

    Up13

---
+++ partner_credentials/models/__init__.py
@@ -0,0 +1,2 @@
+from . import credentials
+from . import res_partner_credentials
---
+++ partner_credentials/models/credentials.py
@@ -0,0 +1,21 @@
+# Copyright
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models, api
+
+class PartnerCredentials(models.Model):
+    _name = 'partner.credentials'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _description = 'Partner Credentials'
+
+    name = fields.Char(string='Nombre')
+    type = fields.Selection([
+        ('odoo', 'Odoo'), ('web', 'Web'), ('other', 'Other')
+    ])
+    partner_id = fields.Many2one('res.partner', string='Partner')
+    user = fields.Char('User')
+    password = fields.Char('Password')
+    public = fields.Boolean('Public')
+    url = fields.Char('Url')
+    active = fields.Boolean('Active', default='True')
+
