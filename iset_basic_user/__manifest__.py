8aab51cd6507c58474b985f303a60bcdbdb34a34
pedro@serincloud.com <pedro@serincloud.com>
2021-06-10 13:15:31 +0200

    basic_user

---
+++ iset_basic_user/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'iSet Basic User',
+    'version': '14.0.1.0.0',
+    'category': '',
+    'description': u"""
+
+""",
+    'author': 'Serincloud',
+    'depends': [
+        'isets',
+        'base_menu_visibility_restriction',
+
+    ],
+    'data': [
+        'data/user.xml',
+    ],
+    'installable': True,
+}
