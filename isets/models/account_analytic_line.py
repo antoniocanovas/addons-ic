d35719ecd77fdd0de472a74de0ada8487d4443cb
pedro@serincloud.com <pedro@serincloud.com>
2021-06-17 16:02:56 +0200

    V3

--- isets/models/account_analytic_line.py
+++ isets/models/account_analytic_line.py
@@ -9,6 +9,13 @@ class AccountAnalyticLine(models.Model):
 
     iset_id = fields.Many2one('isets', store=True)
     iset_so_line_id = fields.Many2one('sale.order.line', store=True)
-    sale_id = fields.Many2one('sale.order', store=True)
+
     type_id = fields.Many2one('working.type', 'Type', store=True)
+
+    @api.depends('project_id')
+    def get_project_sale_id(self):
+        for record in self:
+            record['sale_id'] = record.project_id.sale_id.id
+
+    sale_id = fields.Many2one('sale.order', store=True, compute= get_project_sale_id)
 
