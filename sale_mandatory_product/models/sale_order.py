525f9a67edeae0ac3784fed32f7f80f533128336
Antonio Cánovas <antonio.canovas@ingenieriacloud.com>
2021-07-01 16:49:55 +0200

    label mandatory products

--- sale_mandatory_product/models/sale_order.py
+++ sale_mandatory_product/models/sale_order.py
@@ -7,4 +7,4 @@ _logger = logging.getLogger(__name__)
 class SaleMandatorySaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    sale_mandatory = fields.Boolean(string='Sale Mandatory SO', store=True, default=True)
+    sale_mandatory = fields.Boolean(string='Mandatory products', store=True, default=True)
