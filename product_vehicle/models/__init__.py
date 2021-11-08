e40e790fa2f45c081aef7b2b76df9c8ec311088d
pedroguirao <pedro@serincloud.com>
2021-11-08 17:05:02 +0100

    Update vehicle

--- product_vehicle/models/__init__.py
+++ product_vehicle/models/__init__.py
@@ -4,3 +4,4 @@ from . import crm_vehicle
 
 
 
+
--- product_vehicle/models/crm_vehicle.py
+++ product_vehicle/models/crm_vehicle.py
@@ -7,6 +7,6 @@ _logger = logging.getLogger(__name__)
 class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
-    vehicle_ids = fields.Many2many('product.template',domain=[('is_vehicle','=','True')])
+    vehicle_ids = fields.Many2many('product.template', domain=[('is_vehicle','=','True')])
 
 
