4206a21ca3ab787b4e61e50dc6d08e06f2905fff
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-01-21 02:02:31 +0100

    bug

--- work_extended/models/work_extended_employee.py
+++ work_extended/models/work_extended_employee.py
@@ -4,11 +4,11 @@ import logging
 _logger = logging.getLogger(__name__)
 
 
-class iSetWorkEmployee(models.Model):
-    _name = 'work.extended.employee'
-    _description = 'iSetWorkEmployee'
+class WorkEmployee(models.Model):
+    _name = 'work.employee'
+    _description = 'WorkEmployee'
 
     employee_id = fields.Many2one('hr.employee', 'Employee')
-    work_extended_id = fields.Many2one('work.extended', 'Work Extended')
+    work_id = fields.Many2one('work.extended', 'Work Extended')
     function_ids = fields.Many2many('hr.job')
 
