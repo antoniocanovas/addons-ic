3144576e9c06d6cbe01bae76f62a6fa045789da5
pedroguirao <pedro@serincloud.com>
2022-02-09 16:55:35 +0100

    delete pos_retail

--- pos_retail_standard/models/giftCard/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from. import gift_card
-from. import pos_config
-from. import pos_payment_method
-from. import pos_order
-from. import pos_session
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- pos_retail_standard/static/src/js/Screens/Payment/PaymentMethodButton.js
+++
@@ -1,7 +0,0 @@
-odoo.define('pos_retail_standard.PaymentMethodButton', function (require) {
-    'use strict';
-
-    const PaymentMethodButton = require('point_of_sale.PaymentMethodButton');
-    const Registries = require('point_of_sale.Registries');
-    PaymentMethodButton.template = 'RetailPaymentMethodButton'
-});
