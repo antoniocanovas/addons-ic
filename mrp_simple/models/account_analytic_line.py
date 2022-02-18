bede5392b8b4755c086c58d30d7e4c30ef32e15c
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-02-19 00:26:16 +0100

    new fields and view

---
+++ mrp_simple/models/account_analytic_line.py
@@ -0,0 +1,11 @@
+# Copyright 2021 IC - Pedro Guirao
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import fields, models
+
+
+class MrpBom(models.Model):
+    _inherit = "account.analytic.line"
+
+    mrp_id = fields.Many2one('mrp.production', string='Producción')
+
---
+++ mrp_simple/models/mrp_production.py
@@ -0,0 +1,13 @@
+# Copyright 2021 IC - Pedro Guirao
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import fields, models
+
+
+class MrpBom(models.Model):
+    _inherit = "mrp.production"
+
+    group_id = fields.Many2one('mrp.bom.group', string='Group')
+    timesheet_ids = fields.One2many('account.analytic.line', 'mrp_id', string='Timesheets')
+
+
