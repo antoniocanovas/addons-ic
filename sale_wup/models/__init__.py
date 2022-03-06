fc6e17aa61a1315de21e62a1145a278f7ae18e18
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-03-06 21:55:42 +0100

    consumed 2 wup

--- sale_wup/models/__init__.py
+++ sale_wup/models/__init__.py
@@ -2,6 +2,7 @@ from . import wup_type
 from . import wup_style
 from . import wup_target
 from . import wup_quality
+from . import sale_order
 from . import sale_order_line
 from . import wup_template_line
 from . import wup_template
