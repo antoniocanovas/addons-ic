2ff8a658497d27118e74c02d69871dde06058e00
pedroguirao <pedro@serincloud.com>
2022-03-23 16:58:33 +0100

    product bulk

---
+++ product_bulk/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Product Bulk',
+    'version': '14.0.0.1',
+    'category': 'Stock',
+    'description': u"""
+
+""",
+    'author': 'Serincloud',
+    'depends': [
+        'sale_management',
+        'purchase',
+        'stock',
+        'account',
+        'base_automation',
+    ],
+    'data': ['data/automated_action.xml'],
+    'installable':True,
+}
