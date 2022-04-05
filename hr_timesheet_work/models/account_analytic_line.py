69d669ab9419bb798605b380c5baaa29f34c79a5
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-04-06 01:25:40 +0200

    set_start_stop aal

--- hr_timesheet_work/models/account_analytic_line.py
+++ hr_timesheet_work/models/account_analytic_line.py
@@ -11,5 +11,6 @@ class AccountAnalyticLine(models.Model):
     set_start_stop = fields.Boolean(related='work_sheet_id.set_start_stop', string='Set start & stop time', store=False)
 
     def get_self_id(self):
-        self.self_id = self.env['account.analytic.line'].search([('id', '=', self.id)])
+        for record in self:
+            record['self_id'] = record.id
     self_id = fields.Many2one('account.analytic.line', string='Name ID', compute='get_self_id')
