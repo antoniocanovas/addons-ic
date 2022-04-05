b00e90cef17708781951c4f511448a421477156c
pedroguirao <pedro@serincloud.com>
2022-04-05 16:43:03 +0200

    hr timesheet

--- hr_timesheet_work/models/account_analytic_line.py
+++ hr_timesheet_work/models/account_analytic_line.py
@@ -8,3 +8,7 @@ class AccountAnalyticLine(models.Model):
     _inherit = 'account.analytic.line'
 
     work_sheet_id = fields.Many2one('work.sheet', store=True)
+
+    def get_self_id(self):
+        self.self_id = self.env['account.analytic.line'].search([('id', '=', self.id)])
+    self_id = fields.Many2one('account.analytic.line', string='Name ID', compute='get_self_id')
