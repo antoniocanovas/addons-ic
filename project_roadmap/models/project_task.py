49799bccbae0445f3bbd55e19e6bf3a8f7c83f52
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-06-26 17:09:42 +0200

    project_roadmap o2m from models

--- project_roadmap/models/project_task.py
+++ project_roadmap/models/project_task.py
@@ -8,3 +8,11 @@ class ProjectTask(models.Model):
     _inherit = 'project.task'
 
     roadmap_ids = fields.One2many('project.roadmap', 'task_id', string='Roadmaps', store=True, copy=False)
+    roadmap_count = fields.Integer('Roadmaps', compute="_compute_roadmap_count", store=True)
+    @api.depends("roadmap_ids.active")
+    def _compute_roadmap_count(self):
+        for record in self:
+            total = 0
+            roadmaps = self.env['project.roadmap'].search([('project_id', '=', record.id),('active','in',[True,False])])
+            if roadmaps.ids: total = len(roadmaps.ids)
+        record['roadmap_count'] = total
