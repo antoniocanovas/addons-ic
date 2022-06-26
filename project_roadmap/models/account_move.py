8df7724330cbc7419cb6228be92c7bb836c9242e
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-06-26 18:30:42 +0200

    project_roadmap o2m from models

--- project_roadmap/models/account_move.py
+++ project_roadmap/models/account_move.py
@@ -7,7 +7,10 @@ from odoo import fields, models, api
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    project_roadmap_id = fields.Many2one('project.roadmap',string='Fase')
-
-
-
+    roadmap_count = fields.Integer('Roadmaps', compute="_compute_roadmap_count", store=False)
+    def _compute_roadmap_count(self):
+        for record in self:
+            total = 0
+            roadmaps = self.env['account.move'].search([('move_id', '=', record.id),('active','in',[True,False])])
+            if roadmaps.ids: total = len(roadmaps.ids)
+        record['roadmap_count'] = total
