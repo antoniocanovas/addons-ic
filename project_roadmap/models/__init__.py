75b505a68f88a11673905400658354f3b4b7a8c8
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-06-26 18:55:24 +0200

    project_roadmap o2m from models

--- project_roadmap/models/__init__.py
+++ project_roadmap/models/__init__.py
@@ -2,6 +2,6 @@ from . import project_project
 from . import project_task
 from . import project_roadmap
 from . import account_move
-#from . import sale_order
+from . import sale_order
 #from . import purchase_order
 #from . import stock_picking
