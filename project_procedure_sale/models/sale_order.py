352585a41c66f5759499c9f10d9a863e5083e31b
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-07-24 18:19:22 +0200

    project_procedure_sale

--- project_procedure_sale/models/sale_order.py
+++ project_procedure_sale/models/sale_order.py
@@ -8,8 +8,8 @@ from odoo import fields, models, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    @api.depends('status')
-    def create_tasks_if_procedure_when_sold(self):
+    @api.depends('write_date')
+    def create_procedure_tasks_when_confirmed(self):
         if (self.state == 'sale'):
             for pr in self.project_ids:
                 if (pr.procedure_id.id) and (not pr.task_ids.ids):
