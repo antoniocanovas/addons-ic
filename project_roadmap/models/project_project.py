e73cf9e712752e88b2c6b151ff5a781ce3e3cb30
pedroguirao <pedro@serincloud.com>
2022-08-29 11:49:06 +0200

    roadmap

--- project_roadmap/models/project_project.py
+++ project_roadmap/models/project_project.py
@@ -1,15 +1,22 @@
 # Copyright
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import fields, models, api
+from odoo import fields, models
+
 
 class ProjectProject(models.Model):
-    _inherit = 'project.project'
+    _inherit = "project.project"
 
-    roadmap_count = fields.Integer('Roadmaps', compute="_compute_roadmap_count", store=True)
+    roadmap_count = fields.Integer(
+        "Roadmaps", compute="_compute_roadmap_count", store=True
+    )
+
     def _compute_roadmap_count(self):
         for record in self:
             total = 0
-            roadmaps = self.env['project.roadmap'].search([('project_id', '=', record.id),('active','in',[True,False])])
-            if roadmaps.ids: total = len(roadmaps.ids)
-        record['roadmap_count'] = total
+            roadmaps = self.env["project.roadmap"].search(
+                [("project_id", "=", record.id), ("active", "in", [True, False])]
+            )
+            if roadmaps.ids:
+                total = len(roadmaps.ids)
+        record["roadmap_count"] = total
