2a18005b3aa3355f95c17be681edc96f8c93069e
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-10-14 23:08:50 +0200

    wor_id in analytic.line

--- hr_timesheet_work/models/account_analytic_line.py
+++ hr_timesheet_work/models/account_analytic_line.py
@@ -8,4 +8,5 @@ class AccountAnalyticLine(models.Model):
     _inherit = 'account.analytic.line'
 
     work_sheet_id = fields.Many2one('work.sheet', store=True)
+    work_id = fields.Many2one('timesheet.work', related='work_sheet_id.work_id', store=True)
     set_start_stop = fields.Boolean(related='work_sheet_id.set_start_stop', string='Set start & stop time', store=False)
