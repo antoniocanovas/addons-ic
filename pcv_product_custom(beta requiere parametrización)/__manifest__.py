fe058a1e8e2666e3acbb04cc34a5e82c84e86a44
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-03-13 17:30:32 +0100

    beta pcv

---
+++ pcv_product_custom(beta requiere parametrización)/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "PCV Product Custom",
+    'summary': """
+        Update pricelist from time type.
+        """,
+    'author': "Serincloud SL",
+    'license': 'AGPL-3',
+    'website': "https://ingenieriacloud.com",
+    'category': 'Tools',
+    'version': '16.0.1.0.0',
+    'depends': [
+        'sale_management',
+    ],
+    'data': [
+        'views/views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
