7369cb8d1a7ec71c23eb89d6bfa3d7561d405b5e
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-03-15 18:26:15 +0100

    service bom v0

--- mrp_service_bom/models/product.py
+++ mrp_service_bom/models/product.py
@@ -7,6 +7,9 @@ from odoo import fields, models, api
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
+    def _get_update_sale_price(self):
+        return(False)
+
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
