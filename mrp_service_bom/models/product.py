4a71d7562d005e09c0939f2da1e68ebc7a32ce64
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-03-15 18:25:37 +0100

    service bom v0

--- mrp_service_bom/models/product.py
+++ mrp_service_bom/models/product.py
@@ -8,7 +8,7 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
 
 class ProductProduct(models.Model):
-        _inherit = "product.product"
+    _inherit = "product.product"
 
     service_bom_id = fields.Many2one('mrp.bom', string='Bill of service',
                                      domain="[('product_tmpl_id', '=', product_tmpl_id), '|', ('company_id', '=', False), ('company_id', '=', company_id)]",
