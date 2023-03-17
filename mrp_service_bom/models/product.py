a62b73e0c6b384a2e7fa76a16cc6ff38e65349fe
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-03-17 13:47:36 +0100

    service bom v1

--- mrp_service_bom/models/product.py
+++ mrp_service_bom/models/product.py
@@ -7,5 +7,15 @@ from odoo import fields, models, api
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    def _get_update_sale_price(self):
+    def _get_update_sale_price_from_pt(self):
+        return(False)
+
+class ProductProduct(models.Model):
+    _inherit = "product.product"
+
+    service_bom_id = fields.Many2one('mrp.bom', string='Bill of service',
+                                     domain="[('product_tmpl_id', '=', product_tmpl_id), '|', ('company_id', '=', False), ('company_id', '=', company_id)]",
+                                     )
+
+    def _get_update_sale_price_from_pp(self):
         return(False)
