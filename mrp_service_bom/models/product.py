886fdda2c950543971dfab6c1dc902258c394412
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-03-18 11:30:56 +0100

    service bom v1

--- mrp_service_bom/models/product.py
+++ mrp_service_bom/models/product.py
@@ -10,12 +10,12 @@ class ProductTemplate(models.Model):
     def update_variants_sale_price_from_service_bom(self):
         for product in self.product_variant_ids:
             if product.service_bom_id.id:
-                list_price = 0
+                lst_price = 0
                 for li in product.service_bom_id.bom_line_ids:
-                    list_price += li.product_id.lst_price * li.product_qty
+                    lst_price += li.product_id.lst_price * li.product_qty
                 if product.service_bom_id.product_qty not in [0,1]:
-                    list_price = list_price / product.service_bom_id.product_qty
-                product.write({'lst_price':list_price})
+                    lst_price = lst_price / product.service_bom_id.product_qty
+                product.write({'lst_price':lst_price})
 
 class ProductProduct(models.Model):
     _inherit = "product.product"
@@ -25,10 +25,10 @@ class ProductProduct(models.Model):
 
     @api.depends('service_bom_id')
     def update_sale_price_from_service_bom(self):
-        list_price = 0
+        lst_price = 0
         if self.service_bom_id.id:
             for li in self.service_bom_id.bom_line_ids:
-                list_price += li.product_id.lst_price * li.product_qty
+                lst_price += li.product_id.lst_price * li.product_qty
             if self.service_bom_id.product_qty not in [0,1]:
-                list_price = list_price / self.service_bom_id.product_qty
-        self.lst_price = list_price
+                lst_price = lst_price / self.service_bom_id.product_qty
+        self.write({'lst_price': lst_price})
