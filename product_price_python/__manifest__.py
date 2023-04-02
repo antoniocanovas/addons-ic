5dfcea2e3820b76e29e49e3b0ede2b538192c893
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-04-02 02:31:38 +0200

    price python v0

--- product_price_python/__manifest__.py
+++ product_price_python/__manifest__.py
@@ -1,15 +1,15 @@
 # Copyright 2017 Simone Orsi.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 {
-    'name': 'Website slides private',
+    'name': 'Product price python',
     'version': '16.0.1.0.0',
     'author': 'Serincloud SL',
-    'website': 'https://github.com/OCA/website',
+    'website': 'https://github.com/antoniocanovas',
     'license': 'LGPL-3',
-    'category': 'Website',
-    'summary': 'Slides / courses only for internal users.',
+    'category': 'Sales',
+    'summary': 'Product price updated by python',
     'depends': [
-        'website_blog',
+        'sale',
     ],
     'data': [
         'vies/res_company_views.xml',
