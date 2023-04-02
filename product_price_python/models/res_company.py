6783759131184bb41f4364de1ccf3945ebbb1180
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-04-02 18:20:23 +0200

    price python v0

--- product_price_python/models/res_company.py
+++ product_price_python/models/res_company.py
@@ -3,5 +3,5 @@ from odoo import _, api, fields, models
 class ResCompany(models.Model):
     _inherit = "res.company"
 
-    festivos         = fields.Text(string="Festivos", placeholder="En formato año-mes-día, separado por comas (2023-12-31)")
+    festivos         = fields.Text(string="Festivos")
     festivo_domingo  = fields.Boolean('Festivo el domingo')
