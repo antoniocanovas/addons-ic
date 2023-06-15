214ff1f163fd54e4f8f26f15b26453e1df3ec96c
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-06-15 17:49:40 +0200

    purchase price multicompany

---
+++ external_work_sale_type/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'External Work',
+    'version': '14.0.1.0.1',
+    'category': '',
+    'description': u"""
+External Work
+""",
+    'author': 'Serincloud',
+    'depends': [
+        'external_work',
+        'sale_order_type',
+    ],
+    'data': [
+        'views/external_work_views.xml',
+    ],
+    'installable': True,
+}
