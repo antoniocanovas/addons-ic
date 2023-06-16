9d40a121cd1ffbfa7ff2d8d6930be54cacdae952
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-06-16 19:53:56 +0200

    purchasePriceError

--- purchase_price_update/models/purchase_order_line.py
+++ purchase_price_update/models/purchase_order_line.py
@@ -34,7 +34,7 @@ class PurchasePriceUpdate(models.Model):
                     ('product_id', '=', record.product_id.id),
                     ('product_uom', '=', record.product_uom.id),
                     ('min_qty', '=', 0),
-                    ('company_id', '=', record.company_id.id),
+                    ('company_id', '=', self.env.user.company_id.id),
                 ])
 
                 # Case 'b': Variants disabled => product_tmpl_id ok but no product_id.id in supplierinfo:
@@ -45,8 +45,11 @@ class PurchasePriceUpdate(models.Model):
                         ('product_id', '=', False),
                         ('product_uom', '=', record.product_uom.id),
                         ('min_qty', '=', 0),
-                        ('company_id', '=', record.company_id.id),
+                        ('company_id', '=', self.env.user.company_id.id),
                     ])
+
+                if len(supplierinfo) > 1:
+                    raise Warning('Hay varias tarifas del mismo producto para este proveedor, revísalo')
 
                 if (supplierinfo.id) and (record.product_qty != 0) and \
                         (record.price_unit == supplierinfo.price) and \
@@ -55,7 +58,6 @@ class PurchasePriceUpdate(models.Model):
             record['price_supplierinfo_control'] = control
 
     price_supplierinfo_control = fields.Boolean(string='Supplierinfo Control', compute='get_supplierinfo_control')
-
 
     # Store TODAY standard_price in this purchase.order.line to be used later:
     @api.depends('product_id')
