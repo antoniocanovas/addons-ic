ff07b69f0bfe15c04434f8ae737f934f347e3dd9
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-06-21 19:09:20 +0200

    checklist to project_checklist

--- project_checklist/models/project_checklist_line.py
+++ project_checklist/models/project_checklist_line.py
@@ -9,9 +9,9 @@ class ProjectChecklistLine(models.Model):
     name = fields.Char(required=True)
     sequence = fields.Integer(default=1)
     description = fields.Char()
-    task_id = fields.Many2one('project.task')
-    project_id = fields.Many2one('project.project', related='task_id.project_id', store=True)
     checklist_id = fields.Many2one('project.checklist')
+    task_id = fields.Many2one('project.task', related='checklist_id.task_id')
+    project_id = fields.Many2one('project.project', related='task_id.project_id', store=True)
     state = fields.Selection(
         string='Status', required=True, readonly=True, copy=False,
         tracking=True, selection=[
