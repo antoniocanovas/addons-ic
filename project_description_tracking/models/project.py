64aa8e6a4b92cd212e851dee0b164d26260b2bfa
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-07-05 14:55:24 +0200

    project_des_tracking v0

--- project_description_tracking/models/project.py
+++ project_description_tracking/models/project.py
@@ -13,5 +13,6 @@ class ProjectTask(models.Model):
     _inherit = 'project.task'
 
     def _get_description_text(self):
-        self.description_text = self.description
+        for record in self:
+            record['description_text'] = record.description
     description_text = fields.Text('Description', store=True, tracking=100, compute='_get_description_text')
