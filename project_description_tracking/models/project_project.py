aa9fbaedbd5fed6a54a07ee7f90e6f03553a96f4
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-07-05 14:19:08 +0200

    project_des_tracking v0

--- project_description_tracking/models/project_project.py
+++ project_description_tracking/models/project_project.py
@@ -6,8 +6,4 @@ from odoo import models, fields, api, _
 class ProjectProject(models.Model):
     _name = 'project.project'
 
-    description = fields.Char(string='Description')
-    project_id = fields.Many2one('project.project', string='Project')
-    is_template = fields.Boolean('Template')
-    line_ids = fields.One2many('project.checklist.line', 'checklist_id',
-                               string='CheckLists', required=True)
+    description = fields.Html(tracking=100)
