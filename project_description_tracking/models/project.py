763ef3fc3fdb0c27e7261ce68becd345ced9392f
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-07-05 14:36:15 +0200

    project_des_tracking v0

--- project_description_tracking/models/project.py
+++ project_description_tracking/models/project.py
@@ -7,3 +7,10 @@ class ProjectProject(models.Model):
     _inherit = 'project.project'
 
     description = fields.Html(tracking=100)
+
+
+
+class ProjectTask(models.Model):
+    _inherit = 'project.task'
+
+    description = fields.Html(tracking=100)
