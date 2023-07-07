647fc4ad6fba7e407274255d84a609e1450a75db
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-07-07 19:42:23 +0200

    product_from_set v0

--- product_from_set/models/product_template.py
+++ product_from_set/models/product_template.py
@@ -8,3 +8,6 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     set_template_ids = fields.Many2many('set.template', string='Set templates', store="True",)
+    parent_id = fields.Many2one('product.template', string='Parent set', store=True)
+    set_product_ids  = fields.One2many('product.template','parent_id', string='Set products', store=True)
+
