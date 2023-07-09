e89178b01f224e1b2ce9f218af48286b2234ae94
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-07-09 03:42:42 +0200

    product_from_set v0

--- product_from_set/models/product_template.py
+++ product_from_set/models/product_template.py
@@ -11,6 +11,8 @@ class ProductTemplate(models.Model):
     parent_id = fields.Many2one('product.template', string='Parent set', store=True)
     set_product_ids  = fields.One2many('product.template','parent_id', string='Set products', store=True, readonly=True)
 
+
+
     def _create_set_products(self):
         if not self.default_code:
             raise UserError('Asigna código a este producto para usarlo en las referencias de los surtidos')
@@ -22,23 +24,21 @@ class ProductTemplate(models.Model):
                                                                    'detailed_type':'product',
                                                                    'parent_id':self.id
                                                                    })
-
-
-            # Exista o no deberíamos comprobar si ya tiene la lista de materiales:
+            # Exista o no, comprobamos si ya tiene la lista de materiales:
             if not exist.bom_ids.ids:
                 new_bom = self.env['mrp.bom'].create({'code':code, 'type':'normal', 'product_qty':1,
                                                       'product_tmpl_id':self.id,})
-
             # Mismo chequeo con las líneas bom:
             if not exist.bom_ids.bom_line_ids.ids:
                 # Crear las líneas del BOM tras haber buscado si existen las variantes en SELF:
                 for value in li.line_ids:
                     exist_pp = self.env['product.product'].search([('product_tmpl_id','=',self.id),
-                                                                   ('product_template_variant_value_ids', 'in', li.main_value_ids),
-                                                                   ('product_template_variant_value_ids', 'in', li.value_id),
+                                                                   ('product_template_variant_value_ids', '=', [li.main_value_id.id,li.value_id.id]),
                                                                    ])
                     if exist_pp.id:
                         new_bom_line = self.env['mrp.bom.line'].create({'bom_id':new_bom_id,
                                                                         'product_id':exist_pp.id,
                                                                         'quantity':value.quantity,
                                                                         })
+                    else:
+                        raise UserError('No existe una de las variantes del pack en el producto original.')
