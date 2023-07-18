e91e56d93bc0e72b0910fb71007e3c64b548b9ea
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-07-18 13:09:41 +0200

    ew li product with code

--- l10n_es_factura_canje_tpv/models/factura_canje_taxline.py
+++ l10n_es_factura_canje_tpv/models/factura_canje_taxline.py
@@ -8,7 +8,7 @@ class FacturaCanjeTaxline(models.Model):
     _description = 'Facturas de canje, resumen impuestos'
 
     name = fields.Char('Name', related='fcanje_id.name')
-    tax_id = fields.Many2one('account.tax', string='Impuesto', store=True, copy=False)
-    amount = fields.Monetary('Importe', store=True, copy=False)
-    fcanje_id = fields.Many2one('factura.canje', string='Factura de canje', store=True, copy=False)
+    tax_id = fields.Many2one('account.tax', string='Impuesto', store=True)
+    amount = fields.Monetary('Importe', store=True)
+    fcanje_id = fields.Many2one('factura.canje', string='Factura de canje', store=True)
     currency_id = fields.Many2one('res.currency', string='Moneda', store=True, default=1)
