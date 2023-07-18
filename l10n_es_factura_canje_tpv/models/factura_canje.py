4fcfe16b44cbcc3ba30aba19e4b265f0f02476fa
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-07-19 01:39:27 +0200

    ew li product with code

--- l10n_es_factura_canje_tpv/models/factura_canje.py
+++ l10n_es_factura_canje_tpv/models/factura_canje.py
@@ -23,7 +23,7 @@ class FacturaCanje(models.Model):
 
     tax_line_ids = fields.One2many('factura.canje.taxline', 'fcanje_id', store=True, copy=False)
 
-    @api.onchange('pos_order_ids')
+    @api.depends('pos_order_ids')
     def get_fcanje_taxlines(self):
         for record in self:
             record.tax_line_ids.unlink()
