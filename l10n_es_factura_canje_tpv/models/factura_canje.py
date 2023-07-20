ebb4ff0c9a600f3f638746752b00022add4ca9a9
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-07-20 19:41:57 +0200

    canje totales

--- l10n_es_factura_canje_tpv/models/factura_canje.py
+++ l10n_es_factura_canje_tpv/models/factura_canje.py
@@ -29,15 +29,16 @@ class FacturaCanje(models.Model):
 
     @api.onchange('pos_order_ids')
     def get_fcanje_taxlines(self):
-        monetary_precision = self.env['decimal.precision'].sudo().search([('id', '=', 1)]).digits
+#        monetary_precision = self.env['decimal.precision'].sudo().search([('id', '=', 1)]).digits
         print("DEBUG")
         for l in self.tax_line_ids:
             l.unlink()
 
-        impuestos, amount_total, amount_tax = [], 0, 0
+        impuestos = []
+#        amount_total, amount_tax = 0, 0
         for po in self.pos_order_ids:
-            amount_total += po.amount_total
-            amount_tax += po.amount_tax
+#            amount_total += po.amount_total
+#            amount_tax += po.amount_tax
             for li in po.lines:
                 for tax in li.tax_ids_after_fiscal_position:
                     print("TAX FOR", li.tax_ids_after_fiscal_position)
@@ -53,7 +54,7 @@ class FacturaCanje(models.Model):
             print("id", im._origin.id)
             new = self.env['factura.canje.taxline'].create({'fcanje_id': self.id, 'amount': amount, 'tax_id': im._origin.id})
             print("NEW", new)
-        self.write({'amount_total':amount_total, 'amount_tax':amount_tax, 'amount_subtotal':amount_total - amount_tax})
+#        self.write({'amount_total':amount_total, 'amount_tax':amount_tax, 'amount_subtotal':amount_total - amount_tax})
 
 
     @api.depends('create_date')
