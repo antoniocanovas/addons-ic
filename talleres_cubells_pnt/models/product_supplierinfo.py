3770c6fdb5cb8d755a3dbfc705d5ff7284319da0
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-07-26 13:58:46 +0200

    cubells vacp

--- talleres_cubells_pnt/models/product_supplierinfo.py
+++ talleres_cubells_pnt/models/product_supplierinfo.py
@@ -6,13 +6,13 @@ from datetime import timedelta
 class ProductSupplierinfo(models.Model):
     _inherit = 'product.supplierinfo'
 
-    @api.depends('partner_id','date_start','price','discount')
-    def _get_psi_name(self):
+#    @api.depends('partner_id','date_start','price','discount')
+    def name_get(self):
         for record in self:
             name = record.partner_id.name + str(record.price) + "€, Dto: " + str(record.discount) + "% "
             if record.last_purchase_date:
                 name += str(record.last_purchase_date)
             record['display_name'] = name
-    display_name = fields.Char('Name', store=False, compute='_get_psi_name')
+    display_name = fields.Char('Name', store=False, compute='name_get')
 
-    name = fields.Char('Name', store=False, compute='_get_psi_name')
+    name = fields.Char('Name', store=False, compute='name_get')
