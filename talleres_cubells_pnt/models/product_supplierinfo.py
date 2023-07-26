c34d5050287660074edf8fea88ad659b89d6d22d
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-07-26 13:53:16 +0200

    cubells vacp

--- talleres_cubells_pnt/models/product_supplierinfo.py
+++ talleres_cubells_pnt/models/product_supplierinfo.py
@@ -12,5 +12,7 @@ class ProductSupplierinfo(models.Model):
             name = record.partner_id.name + str(record.price) + "€, Dto: " + str(record.discount) + "% "
             if record.last_purchase_date:
                 name += str(record.last_purchase_date)
-            record['name'] = name
+            record['display_name'] = name
+    display_name = fields.Char('Name', store=False, compute='_get_psi_name')
+
     name = fields.Char('Name', store=False, compute='_get_psi_name')
