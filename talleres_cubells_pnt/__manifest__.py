5333eba79427ec52138b58d2cc518437b2d01922
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-07-26 13:17:42 +0200

    cubells vacp

---
+++ talleres_cubells_pnt/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Personalizaciones para Talleres Cubells',
+    'version': '14.0.1.0.0',
+    'summary': 'Personalizaciones',
+    'author': 'Punt Sistemes S.L.U.',
+    'contributors': ['Isaac Gallart <igallart@puntsistemes.es>'],
+    'website': 'http://www.puntsistemes.es',
+    'license': 'AGPL-3',
+    'category': 'Custom',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'depends': [
+        'purchase',
+        'sale',
+        'stock',
+        'account',
+        'maintenance',
+        'hr_maintenance',
+        'purchase_discount',
+    ],
+    'data': [
+            'security/ir.model.access.csv',
+            #'views/templates.xml',
+            'views/purchase_view.xml',
+            'views/stock_view.xml',
+            'views/sale_order.xml',
+            'views/maintenance.xml',
+            'views/model.xml',
+            'views/fuel.xml',
+            'views/invoice.xml',
+    ]
+}
---
+++ talleres_cubells_pnt/models/product_supplierinfo.py
@@ -0,0 +1,17 @@
+
+from odoo import models, api, fields
+from datetime import timedelta
+
+
+class ProductSupplierinfo(models.Model):
+    _inherit = 'product.supplierinfo'
+
+    @api.depends('partner_id','date_start','price','discount')
+    def _get_psi_name(self):
+        for record in self:
+            name = record.partner_id.name + str(record.price) + "€, Descuento: " + str('record.discount') + "% "
+            last_purchase_date = ""
+            if record.last_purchase_date:
+                name += str(record.last_purchase_date)
+            record['name'] = name
+    name = fields.Char('Name', store=False, compute='_get_psi_name')
