c81faf2ab1eb1f5b1ae1b23f2700af05f0cca24d
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-08-02 02:53:49 +0200

    project tracking v0

--- project_tag_tracking/models/project_project.py
+++ project_tag_tracking/models/project_project.py
@@ -5,4 +5,4 @@ from odoo import models, fields, api, _
 class ProjectProject(models.Model):
     _inherit = 'project.project'
 
-    tag_ids = fields.Many2many(tracking=100)
+    tag_ids = fields.Many2many(tracking=False)
