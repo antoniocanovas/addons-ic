e975a5bfcc28219531ff8f8c7e060027771dc35b
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-09-26 16:14:42 +0200

    project task tracking dif

---
+++ project_tag_tracking_dif/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Project and task tags traking differential',
+    'version': '16.0.1.0.0',
+    'category': 'Project/Project',
+    'summary': "Use Tags to follow your workflow",
+    'description': "Every change in task tags are tracked to chatter, only with difference not all tags previous. "
+                   "It's recommended when there are a lot of tags",
+    'author': 'Serincloud',
+    'company': 'Serincloud',
+    'maintainer': 'Serincloud',
+    'website': 'https://www.ingenieriacloud.com',
+    'depends': [
+        'project',
+        'base_automation',
+    ],
+    'data': [
+        'data/automatic_actions.xml',
+    ],
+    'assets': {},
+
+    'images': ['static/description/icon.png'],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
