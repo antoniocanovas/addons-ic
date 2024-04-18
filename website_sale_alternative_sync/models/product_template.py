700e43e821fe080548269749fd3ca2d48c4d68a9
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-04-18 18:41:57 +0200

    website sale alternatives sync

---
+++ website_sale_alternative_sync/models/product_template.py
@@ -0,0 +1,10 @@
+# Copyright 2021 Pedro Guirao - Ingenieriacloud.com
+
+
+from odoo import fields, models, api
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    last_product_aternative_ids = fields.Many2Many('product.template', string='Last alternative products', store="True",)
