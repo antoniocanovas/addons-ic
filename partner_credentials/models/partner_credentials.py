f50cab4d78c8bff12a07ffd6ee68f9482f65b445
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-04-29 19:54:23 +0200

    credentials v2

--- partner_credentials/models/partner_credentials.py
+++ partner_credentials/models/partner_credentials.py
@@ -36,8 +36,8 @@ class PartnerCredentials(models.Model):
         for record in self:
             admin_group = self.env.ref('partner_credentials.admin_credentials_group')
             edit = False
-            if (self.user == record.create_uid): edit = True
-#            if (admin_group.users.ids) and (self.user in admin_group.users): edit = True
+            if (self.env.user == record.create_uid): edit = True
+            if (admin_group.users.ids) and (self.env.user in admin_group.users): edit = True
             record['user_can_edit'] = edit
     user_can_edit = fields.Boolean('Edit', compute='_user_can_edit')
 
