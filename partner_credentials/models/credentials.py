c5ff765d8f7d0fc858f8fb2cfca13812132ca62f
pedroguirao <pguirao@puntsistemes.es>
2024-04-29 15:25:07 +0000

    VPUNT

--- partner_credentials/models/credentials.py
+++ partner_credentials/models/credentials.py
@@ -3,20 +3,33 @@
 
 from odoo import fields, models, api
 
-class PartnerCredentials(models.Model):
-    _name = 'partner.credentials'
-    _inherit = ['mail.thread', 'mail.activity.mixin']
-    _description = 'Partner Credentials'
 
-    name = fields.Char(string='Nombre', required=True)
-    type = fields.Selection([
-        ('odoo', 'Odoo'), ('web', 'Web'), ('other', 'Other')
-    ],  required=True)
-    partner_id = fields.Many2one('res.partner', string='Partner')
-    user = fields.Char('User')
-    password = fields.Char('Password')
-    public = fields.Boolean('Public')
-    url = fields.Char('Url')
-    active = fields.Boolean('Active', default='True')
+class PartnerCredentials(models.Model):
+    _name = "partner.credentials"
+    _inherit = ["mail.thread", "mail.activity.mixin"]
+    _description = "Partner Credentials"
+
+    name = fields.Char(string="Nombre", required=True)
+    type = fields.Selection(
+        [("odoo", "Odoo"), ("web", "Web"), ("other", "Other")], required=True
+    )
+    partner_id = fields.Many2one("res.partner", string="Partner")
+    user = fields.Char("User")
+    password = fields.Char("Password")
+    public = fields.Boolean("Public")
+    url = fields.Char("Url")
+    active = fields.Boolean("Active", default="True")
     description = fields.Text("Description")
 
+    def action_view_password(self):
+        action = {
+            "type": "ir.actions.client",
+            "tag": "display_notification",
+            "params": {
+                "title": "Title",
+                "message": "message",
+                "sticky": False,
+                "next": {"type": "ir.actions.act_window_close"},
+            },
+        }
+        return action
--- partner_credentials/models/res_partner_credentials.py
+++ partner_credentials/models/res_partner_credentials.py
@@ -5,15 +5,18 @@ from odoo import fields, models, api
 
 
 class PartnerCredentialsFields(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
     def _get_credentials(self):
-        results = self.env['partner.credentials'].search([('partner_id', '=', self.id)])
+        results = self.env["partner.credentials"].search([("partner_id", "=", self.id)])
         self.credentials_count = len(results)
 
-    credentials_count = fields.Integer('Credentials', compute=_get_credentials, store=False)
+    credentials_count = fields.Integer(
+        "Credentials", compute=_get_credentials, store=False
+    )
 
     def action_view_credentials(self):
-        action = self.env.ref(
-            'partner_credentials.action_partner_credentials').read()[0]
+        action = self.env.ref("partner_credentials.action_partner_credentials").read()[
+            0
+        ]
         return action
