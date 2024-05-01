e0ded8f5e513b553e78e7a42523515ba2d6e494d
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-05-01 10:02:44 +0200

    website sale accesories sync

--- website_sale_alternative_sync/models/product_template.py
+++ website_sale_alternative_sync/models/product_template.py
@@ -7,9 +7,17 @@ from odoo import fields, models, api
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    last_product_aternative_ids = fields.Many2many(comodel_name='product.template',
-                                                   relation='product_lead_rel',
+    last_aternative_product_ids = fields.Many2many(comodel_name='product.template',
+#                                                   relation='product_lead_rel',
+                                                   relation='product_alternative_rel',
                                                    column1='product1_id',
                                                    column2='product2_id',
                                                    string='Last alternative products',
+                                                   store="True",)
+
+    last_accesory_product_ids = fields.Many2many(comodel_name='product.template',
+                                                   relation='product_accesory_rel',
+                                                   column1='product1_id',
+                                                   column2='product2_id',
+                                                   string='Last accesory products',
                                                    store="True",)
