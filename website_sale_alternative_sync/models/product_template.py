4a0f29a44b6ca595f67ebffaecfeeedf0a4a5903
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-05-01 17:27:57 +0200

    website sale accesories sync

--- website_sale_alternative_sync/models/product_template.py
+++ website_sale_alternative_sync/models/product_template.py
@@ -8,7 +8,7 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     last_aternative_product_ids = fields.Many2many(comodel_name='product.template',
-#                                                   relation='product_lead_rel',
+                                                   #                                                   relation='product_lead_rel',
                                                    relation='product_alternative_rel',
                                                    column1='product1_id',
                                                    column2='product2_id',
@@ -16,8 +16,8 @@ class ProductTemplate(models.Model):
                                                    store="True",)
 
     last_accessory_product_ids = fields.Many2many(comodel_name='product.template',
-                                                   relation='product_accessory_rel',
-                                                   column1='product1_id',
-                                                   column2='product2_id',
-                                                   string='Last accessory products',
-                                                   store="True",)
+                                                  relation='product_accessory_rel',
+                                                  column1='product1_id',
+                                                  column2='product2_id',
+                                                  string='Last accessory products',
+                                                  store="True",)
