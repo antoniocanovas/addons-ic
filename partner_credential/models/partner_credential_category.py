5d615f490579b2580eb739bffc004b15107c2e22
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-06-04 18:21:11 +0200

    credentials v3

--- partner_credential/models/partner_credential_category.py
+++ partner_credential/models/partner_credential_category.py
@@ -11,3 +11,9 @@ class PartnerCredentialCategory(models.Model):
     name = fields.Char(string='Name', required=True)
     department_ids = fields.Many2many("hr.department", string="Departments")
     logo = fields.Binary('Logo')
+
+    def _get_credential_category_count(self):
+        for record in self:
+            credentials = self.env['partner.credential'].search([('category_id','=',record.id)])
+            record['credential_count'] = len(credentials)
+    credential_count = fields.Integer("Credential count", compute='_get_credential_category_count')
