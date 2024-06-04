244fdcd64985d9528a6aa9d4889116cd08e2add8
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-06-04 18:38:43 +0200

    credentials v3

--- partner_credential/models/partner_credential_category.py
+++ partner_credential/models/partner_credential_category.py
@@ -17,3 +17,15 @@ class PartnerCredentialCategory(models.Model):
             credentials = self.env['partner.credential'].search([('category_id','=',record.id)])
             record['credential_count'] = len(credentials)
     credential_count = fields.Integer("Credential count", compute='_get_credential_category_count')
+
+    def action_view_credentials(self):
+        action = self.env['ir.actions.act_window'].with_context({'active_id': self.id})._for_xml_id('partner_credential.act_credential_category_2_credential_all')
+        action['display_name'] = _("%(name)s", name=self.name)
+        context = action['context'].replace('active_id', str(self.id))
+        context = ast.literal_eval(context)
+        context.update({
+            'create': self.active,
+            'active_test': self.active
+            })
+        action['context'] = context
+        return action
