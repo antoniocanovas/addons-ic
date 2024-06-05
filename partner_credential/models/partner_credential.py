20f6dff61dca7e8137db917cf58503bbb610d602
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-06-05 08:28:08 +0200

    credentials v3

--- partner_credential/models/partner_credential.py
+++ partner_credential/models/partner_credential.py
@@ -30,8 +30,8 @@ class PartnerCredential(models.Model):
     )
     department_ids = fields.Many2many(
         "hr.department", string="Addtional deps.", tracking=100,
-        help='Additional departments with access to this credential (in addition to those in the category).'
-             'To assign an specific user, set as follower.'
+        help='Additional departments with access to this credential (in addition to those in the category). "\n"'
+             'NOTE: If you need to assign an specific user, set as follower.'
     )
 
     key_2fa_secret = fields.Char("2FA Secret")
