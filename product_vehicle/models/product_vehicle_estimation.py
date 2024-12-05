987ae9d2911ddec225533eaf5de79b582cec6759
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-12-05 17:42:30 +0100

    UPD TRANSLATION VEHICLE

--- product_vehicle/models/product_vehicle_estimation.py
+++ product_vehicle/models/product_vehicle_estimation.py
@@ -10,7 +10,7 @@ class ProductVehicleEstimation(models.Model):
 
     product_vehicle_id = fields.Many2one('product.template', string="Vehicle")
     product_id = fields.Many2one('product.product', string="Related Product", required=True)
-    amount = fields.Float(string='Amount')
+    amount = fields.Float(string='Net Amount')
     invoiced = fields.Boolean("Invoiced")
     date = fields.Date(string='Date')
 
